870ae737f5c6e64c07f0ec3f92159a9772a5e0e8
tbreimer <breimert@union.edu>
2024-05-27 23:24:35 -0400

    forgot one

--- parser/src/parser_core.rs
+++ parser/src/parser_core.rs
@@ -1,6 +1,5 @@
-/*
-Converts tokens into an AST and creates a symbol table stack
-*/
+//! The driver for the parsing process, uses the method of recursive descent to systematically iterate through 
+//! tokens and routes to appropriate helper methods in the parser to construct an abstract syntax tree.
                                  
 use common::{ 
     ast::{
@@ -18,6 +17,27 @@ pub struct Parser {
 }
 
 impl Parser {
+    /// Creates a new `Parser` instance with the given input tokens.
+    ///
+    /// This initializer sets up a `Parser` by accepting a vector of tokens and initializing the
+    /// current token index to 0.
+    ///
+    /// # Parameters
+    ///
+    /// - `input`: A vector of `Token` representing the sequence of tokens to be parsed.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `Parser` instance ready to parse the provided tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use lexer::token::Token;
+    /// use parser::Parser;
+    /// let tokens = vec![Token::IDENTIFIER(vec!['a', 'b', 'c']), Token::EQUAL, Token::NUMBER(vec!['1'])];
+    /// let parser = Parser::new(tokens);
+    /// ```
     fn new(input: Vec<Token>) -> Self {
         Self {
             input,
@@ -25,7 +45,28 @@ impl Parser {
         }
     }
 
-    /// Parses an input of tokens into an AST, or returns a vector of errors
+    /// Parses an input of tokens into an AST using recursive descent parsing.
+    /// Iterates through tokens and routes to appropriate helper methods to construct an AST.
+    ///
+    /// # Parameters
+    ///
+    /// - `input`: A vector of `Token` representing the input to be parsed.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result<AST, Vec<ErrorType>>` containing the constructed AST if successful, 
+    /// or a vector of `ErrorType` if there are parsing errors.
+    ///
+    /// # Errors
+    ///
+    /// - Returns a vector of errors if there are issues during parsing, such as unexpected tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let tokens: Vec<Token> = vec![/* tokens */];
+    /// let ast = Parser::parse(tokens);
+    /// ```
     pub fn parse(input: Vec<Token>) -> Result<AST, Vec<ErrorType>> {
         let mut parser = Parser::new(input);
         let mut root_children: Vec<ASTNode> = Vec::new();  
@@ -61,17 +102,62 @@ impl Parser {
         self.current -= 1;
     }
 
-    /// Gets the current input vector
+    /// Gets the current input vector.
+    ///
+    /// # Returns
+    ///
+    /// Returns a vector of `Token` representing the input.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use parser::Parser;
+    /// let parser = Parser::new(vec![/* tokens */]);
+    /// let input = parser.get_input();
+    /// assert!(!input.is_empty());
+    /// ```
     pub fn get_input(&mut self) -> Vec<Token> {
         self.input.clone()
     }
 
-    /// Gets the current position in the input vector
+    /// Gets the current position in the input vector.
+    ///
+    /// # Returns
+    ///
+    /// Returns the current position as a `usize`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use parser::Parser;
+    /// let parser = Parser::new(vec![/* tokens */]);
+    /// let current_position = parser.get_current();
+    /// assert_eq!(current_position, 0);
+    /// ```
     pub fn get_current(&mut self) -> usize {
         self.current.clone()
     }
 
-    /// Consumes a token if the expected token matches the token
+    /// Consumes a token if the expected token matches the current token in the input.
+    ///
+    /// # Parameters
+    ///
+    /// - `expected_token`: The `Token` that is expected to be consumed.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Result<(), ErrorType>` indicating whether the token was successfully consumed.
+    ///
+    /// # Errors
+    ///
+    /// - Returns an error if the expected token does not match the current token or if there is no token to consume.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![Token::LPAREN]);
+    /// let result = parser.consume_token(Token::LPAREN);
+    /// ```
     pub fn consume_token(&mut self, expected_token: Token) -> Result<(), ErrorType> {
         if let Some(token) = self.get_input().get(self.get_current()) {
             if *token == expected_token {
@@ -85,7 +171,19 @@ impl Parser {
         }
     }
     
-    /// Peeks at the token that's next (self.current + 1)
+    /// Peeks at the next token in the input (current position + 1).
+    ///
+    /// # Returns
+    ///
+    /// Returns an `Option<Token>` containing the next token if available, or `None` if there is no next token.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![Token::LPAREN, Token::RPAREN]);
+    /// let next_token = parser.peek_token();
+    /// assert_eq!(next_token, Some(Token::RPAREN));
+    /// ```
     pub fn peek_token(&mut self) -> Option<Token> {
         if self.get_current() < self.get_input().len() {
             self.get_input().get(self.get_current() + 1).cloned()
@@ -95,7 +193,20 @@ impl Parser {
         }
     }
 
-    /// Peeks at the token that's before this one (self.current - 1)
+    /// Peeks at the previous token in the input (current position - 1).
+    ///
+    /// # Returns
+    ///
+    /// Returns an `Option<Token>` containing the previous token if available, or `None` if there is no previous token.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![Token::LPAREN, Token::RPAREN]);
+    /// parser.consume_token(Token::LPAREN).unwrap();
+    /// let last_token = parser.peek_last_token();
+    /// assert_eq!(last_token, Some(Token::LPAREN));
+    /// ```
     pub fn peek_last_token(&mut self) -> Option<Token> {
         if self.get_current() > 0 {
             self.get_input().get(self.get_current() - 1).cloned()
@@ -116,12 +227,31 @@ impl Parser {
         }
     }    
 
-    /// Steps the current token position forward.
+    /// Steps the current token position forward by 1.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![/* tokens */]);
+    /// parser.step_current_forward();
+    /// ```
     pub fn step_current_forward(&mut self) {
         self.current += 1;
     }
 
     /// Returns a null expression. Used to ignore tokens that aren't legal on their own but may be part of a larger expression.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result<Option<ASTNode>, Vec<ErrorType>>` containing a `NoExpression` AST node.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![/* tokens */]);
+    /// let node = parser.no_expression();
+    /// assert!(node.is_ok());
+    /// ```
     pub fn no_expression(&mut self) -> Result<Option<ASTNode>, Vec<ErrorType>> {
         self.step_current_forward();
         Ok(Some(ASTNode::new(SyntaxElement::NoExpression)))
@@ -129,8 +259,23 @@ impl Parser {
     }
 
 
-    /// Entry point to the main parsing logic. Serves as a way to match the current token type to the file/expression we want to parse
-    // TODO Need to actually return errors here
+    /// Entry point to the main parsing logic. Routes the current token to the appropriate parsing method based on token type.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result<Option<ASTNode>, Vec<ErrorType>>` containing the parsed AST node or errors encountered during parsing.
+    ///
+    /// # Errors
+    ///
+    /// - Returns a vector of errors if there are issues during parsing, such as unexpected tokens or parsing failures.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut parser = Parser::new(vec![Token::IF]);
+    /// let result = parser.parse_router();
+    /// assert!(result.is_ok());
+    /// ```
     pub fn parse_router(&mut self) -> Result<Option<ASTNode>, Vec<ErrorType>> {
         if self.get_current() < self.get_input().len() {
             match self.get_input().get(self.get_current()) {
