72cb49784c42b98b017b09a6b08df6ca66513396
livigwinnett <124712098+livigwinnett@users.noreply.github.com>
2024-06-03 23:39:36 -0400

    Update lib.rs

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,17 @@
-/// Core of the LLVM IR generation process
+/// Lexer Module
+///
+/// This module converts our source code into tokens to be sent to the parser.
+/// Though this module is simple, it is important for efficiency and allowing the other modules to be simpler. 
+///
+/// ## Structure
+///
+/// The `lexer` module is organized into submodules:
+///
+/// - `core`: Core takes source code as input and outputs tokens and ignores comments and whitespace. 
+/// - 'token': Tokens are the base unit of our compiler. This module contains accepted tokens.
+
+/// Core of the Lexer
 pub mod lexer_core;
 
+/// Lists accepted tokens
 pub mod token;
